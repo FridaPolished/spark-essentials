0f8fc97841cb141532f5990b850bd0bd212b26c8
FridaPolished <fridapulido@gmail.com>
2021-08-17 22:26:26 -0700

    Add initial exercises for Spark data frames

---
+++ src/main/scala/part1recap/ScalaRecap.scala
@@ -0,0 +1,11 @@
+package part1recap
+
+object ScalaRecap extends App {
+  val b : Boolean = false
+
+  val e = if(2 > 3) "bigger" else "smaller"
+
+  val theUnit = println("hi") //Unit
+
+  def myFunc(x: Int) = 42
+}
